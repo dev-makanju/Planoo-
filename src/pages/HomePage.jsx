05206bf09d927e6b862b60e8528aa6fd7b271568
miracool <makurseme@gmail.com>
2024-01-02 22:13:39 +0100

    added an accordion section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Banner from '../components/Home/Banner';
 import Header from '../components/Home/header/Header';
 import Footer from '../components/Home/Footer/Footer';
-import PlannoCard from '../components/cards/PlannoCard';
+import PlannoCard from '../components/Home/cards/PlannoCard';
+import Accordion from '../components/Home/accordion/accordion'
+import '../components/Home/accordion/accordion.css';
 
 const HomePage = () => {
   return (
@@ -10,6 +12,15 @@ const HomePage = () => {
       <Header/>
       <Banner/>
       <PlannoCard/>
+      <div className='accordion-wrapper'>
+        <div className='accordion'>
+          <Accordion/>
+        </div>
+        <div className='accordion'>
+          <h2>Frequently Asked Questions</h2>
+          <button className='btn-accordion'>Have Other Questions</button>
+        </div>
+      </div>
       <Footer/>
    </>
   )
